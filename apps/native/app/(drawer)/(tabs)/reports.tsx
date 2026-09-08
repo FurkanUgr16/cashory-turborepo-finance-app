7ef4cd548fc222a9bac601a882e96400f4734265
Furkan Uğur <ugurfurkan1645@gmail.com>
2026-09-08 20:12:49 +0300

    report screen

--- apps/native/app/(drawer)/(tabs)/reports.tsx
+++ apps/native/app/(drawer)/(tabs)/reports.tsx
@@ -1,10 +1,5 @@
-import { View, Text } from "react-native";
-import React from "react";
+import ReportsTemplate from "@/components/templates/report-template";
 
 export default function Reports() {
-  return (
-    <View>
-      <Text>Reports</Text>
-    </View>
-  );
+  return <ReportsTemplate />;
 }
